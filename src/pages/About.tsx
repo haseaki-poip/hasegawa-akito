b9042b7ac0c92f389152f8093e29f17458070f29
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-12 02:09:59 +0900

    header作成

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,11 @@
+import Header from "src/components/common/Header";
 import SelfIntroduction from "src/components/view/SelfIntroduction";
 
 const About = () => {
   return (
     <div>
       <SelfIntroduction />
+      <Header pageName="about" />
     </div>
   );
 };
