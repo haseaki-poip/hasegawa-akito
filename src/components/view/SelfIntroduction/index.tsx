487dc539b5bb437f54ff88301953ef426091b6f2
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-11 18:52:13 +0900

    my-image挿入

--- src/components/view/SelfIntroduction/index.tsx
+++ src/components/view/SelfIntroduction/index.tsx
@@ -1,9 +1,17 @@
 import Headline from "src/components/common/Headline";
+import myImage from "src/assets/images/hasegawa.png";
+import "./SelfIntroduction.css";
 
 const SelfIntroduction = () => {
   return (
     <div id="about">
       <Headline headlineName="about" />
+      <div className="introduction-content">
+        <div className="my-image">
+          <img src={myImage} alt="myImage" />
+        </div>
+        <div className="self-introduction">a</div>
+      </div>
     </div>
   );
 };
