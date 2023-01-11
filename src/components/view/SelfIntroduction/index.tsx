1feb1f7630948afab579b0a4a31e70429af0ca6b
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-11 20:03:51 +0900

    自己紹介文作成

--- src/components/view/SelfIntroduction/index.tsx
+++ src/components/view/SelfIntroduction/index.tsx
@@ -8,9 +8,20 @@ const SelfIntroduction = () => {
       <Headline headlineName="about" />
       <div className="introduction-content">
         <div className="my-image">
-          <img src={myImage} alt="myImage" />
+          <div className="flex-column-center">
+            <img src={myImage} alt="myImage" />
+            <h2>Engineer</h2>
+          </div>
         </div>
-        <div className="self-introduction">a</div>
+        <div className="self-introduction">
+          <h1>長谷川 祥士</h1>
+          <h3>所属: 名古屋工業大学大学院</h3>
+          <h3>趣味: スノボー, ドライブ, アプリ開発</h3>
+          <p>
+            webアプリ開発を勉強しており、ハッカソンに出場し入賞することを目標としています。
+            <span>UI</span> <span>UX</span>の向上を心がけ精進しています!!
+          </p>
+        </div>
       </div>
     </div>
   );
