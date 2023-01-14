4d9b58b288fa9eb3027deff57d26fa541c4e88cc
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-15 00:09:58 +0900

    routingを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/common/Footer";
 import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
 
 const App = () => {
   return (
     <div className="App">
-      <About />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+
       <Footer />
     </div>
   );
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+  return <div>portfolio</div>;
+};
+
+export default Portfolio;
