11181d2efa0d23648a25cc36d8ab88035da235ea
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-15 16:12:51 +0900

    スクロールを暗示

--- src/components/view/PortfolioGallery/index.tsx
+++ src/components/view/PortfolioGallery/index.tsx
@@ -22,14 +22,14 @@ const PortfolioGallery = () => {
 
     if (scroll_in_span <= fadeInStart) {
       const opacity = 1 - (fadeInStart - scroll_in_span) / fadeInStart;
-      const translateY = ((fadeInStart - scroll_in_span) / fadeInStart) * 100;
+      const translateY = ((fadeInStart - scroll_in_span) / fadeInStart) * 200;
       return {
         opacity: `${opacity}`,
         transform: `translateY(${translateY}px)`,
       };
     } else if (scroll_in_span >= fadeOutStart) {
       const opacity = 1 - (scroll_in_span - fadeOutStart) / fadeInStart;
-      const translateY = ((scroll_in_span - fadeOutStart) / fadeInStart) * 100;
+      const translateY = ((scroll_in_span - fadeOutStart) / fadeInStart) * 200;
       return {
         opacity: `${opacity}`,
         transform: `translateY(${translateY}px)`,
@@ -41,8 +41,27 @@ const PortfolioGallery = () => {
     return Math.floor(scrollPosition / span); // 切り捨て
   };
 
+  const scrollSignClassName = (() => {
+    if (scrollPosition === 0) return "scroll-up-sign";
+    if (scrollPosition > maxScroll + 100) return "scroll-down-sign";
+    return "";
+  })();
+
   return (
     <div className="portfolio-content" style={portfolioContentStyle}>
+      <div className={"scroll-sign " + scrollSignClassName}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <g>
+            <path
+              d="M395.773,225.883c-33.466,36.809-42.561,44.202-42.561,44.202c-6.183,5.902-15.278,7.543-23.136,4.183
+		c-7.849-3.368-12.945-11.085-12.945-19.627V36.808l-0.509,0.044C313.421,15.988,295.39,0,273.623,0
+		c-24.022,0-43.5,19.478-43.5,43.508v120.467l-122.282,49.323c-36.414,15.611-54.69,56.585-41.982,94.104l46.64,141.023V512h211.324
+		v-63.575c0,0,109.214-164.772,120.467-180.717C464.373,239.275,429.231,189.075,395.773,225.883z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+
       {scrollPosition <= maxScroll ? (
         <div>
           <BackMoveObject />
