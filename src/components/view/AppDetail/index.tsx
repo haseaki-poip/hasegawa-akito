c49721b3f79fc081d13245ea13f92cfdca945b6b
Hasegawa-Akito <h.akito1999@gmail.com>
2023-01-23 01:23:38 +0900

    コンポーネントを跨いだクラス名被りを解消

--- src/components/view/AppDetail/index.tsx
+++ src/components/view/AppDetail/index.tsx
@@ -10,10 +10,9 @@ const PortfolioDetail = ({ portfolioNumber }: Props) => {
 
   return (
     <div className="portfolio-detatil">
-      <div className="close">
+      <a href="/portfolio" className="close">
         <svg
           stroke="currentColor"
-          fill="none"
           viewBox="0 0 24 24"
           height="48"
           width="48"
@@ -22,10 +21,10 @@ const PortfolioDetail = ({ portfolioNumber }: Props) => {
           <line x1="18" y1="6" x2="6" y2="18"></line>
           <line x1="6" y1="6" x2="18" y2="18"></line>
         </svg>
-      </div>
+      </a>
       <div className="detail-content">
         <div className="app-introduction">
-          <div className="introduction-content">
+          <div className="app-introduction-content">
             <h2>{portfolioData.portfolio_name}</h2>
             <p>{portfolioData.portfolio_introduction}</p>
             <p>{portfolioData.sub__introduction}</p>
